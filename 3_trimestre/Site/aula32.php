3b0a464a9152b600f9842bed0bc4248a65b3da8b
Maycon Willian <mayconwas2018@gmail.com>
2018-11-12 17:44:01 -0200

    Template do site

---
+++ 3_trimestre/Site/aula32.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 32: Criando sites com PHP</title>
+  </head>
+  <body>
+
+    <?php include("menu.php"); ?>
+
+    <h2>Exemplo de Site no PHP</h2>
+
+    <p>Acrecentar informações do HTML dentro do PHP</p>
+
+  </body>
+</html>
---
+++ 3_trimestre/Site/menu.php
@@ -0,0 +1,7 @@
+<ul>
+  <li> <a href="#">Pagina inicial</a> </li>
+  <li> <a href="#">Sobre</a> </li>
+  <li> <a href="#">Contato</a> </li>
+  <li> <a href="#">cadastro</a> </li>
+  <li> <a href="#">Login</a> </li>
+</ul>
